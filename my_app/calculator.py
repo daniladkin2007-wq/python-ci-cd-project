e332fe8b850be7a3d155ecbe3d1c9f281eafb19a
daniladkin2007-wq <daniladkin2007-wq@github.com>
2026-05-17 15:00:55 +0300

    Initial commit: calculator with tests, flake8, Dockerfile

---
+++ my_app/calculator.py
@@ -0,0 +1,20 @@
+def add(a, b):
+    """Возвращает сумму двух чисел"""
+    return a + b
+
+
+def subtract(a, b):
+    """Возвращает разность двух чисел"""
+    return a - b
+
+
+def multiply(a, b):
+    """Возвращает произведение двух чисел"""
+    return a * b
+
+
+def divide(a, b):
+    """Возвращает частное двух чисел"""
+    if b == 0:
+        raise ValueError("Деление на ноль невозможно")
+    return a / b
